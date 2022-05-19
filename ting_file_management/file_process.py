e15c9ecc9e3658d7d28bcbcf2cb795b6e27429c7
Pop-Os <ivan@pop-os.localdomain>
2022-05-18 21:01:12 -0300

    requisitos 1, 2 e 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,38 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+def process(path_file: str, instance: Queue):
+    object = {}
+
+    object["nome_do_arquivo"] = path_file
+    object["qtd_linhas"] = len(txt_importer(path_file))
+    object["linhas_do_arquivo"] = txt_importer(path_file)
+
+    print(object, file=sys.stdout)
+
+    for file in range(len(instance)):
+        if (instance.search(file) == object):
+            return object
+
+    instance.enqueue(object)
+
+    return object
+
+
+def remove(instance: Queue):
+    if (len(instance) == 0):
+        print("Não há elementos", file=sys.stdout)
+        return
+
+    file = instance.search(0)["nome_do_arquivo"]
+
+    print(f"Arquivo {file} removido com sucesso", file=sys.stdout)
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        process(instance.search(position)["nome_do_arquivo"], instance)
+    except IndexError:
+        print('Posição inválida', file=sys.stderr)
